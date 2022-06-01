ddd0f178b22aea80bd5c2083c204034c4a87d522
lin12138 <44135942+lin12138@users.noreply.github.com>
2022-06-01 23:03:08 +0800

    Delete cmd_win directory

--- cmd_win/cmd_line.py
+++
@@ -1,9 +0,0 @@
-from eprogress import LineProgress
-
-def CreateProgress(title,max_l):
-    line_progress = LineProgress(title='qweqweqew',total=100,symbol='#')
-    for i in range(101):
-        line_progress.update(i*2)
-        time.sleep(0.05)
-
-
